53c2a31fc059b79cca2e1e09aba26b17a08f15fc
Jasmine Ly <jasminely428@gmail.com>
2022-03-20 19:12:59 -0400

    rwear

---
+++ extending_script.js
@@ -0,0 +1,43 @@
+window.onload = () => { //load when the window running the code is open
+  render(); //render function called, elaborated more below
+};
+
+const models = [  //declariung and initializing a model
+  {
+    url: './assets/solgaleo_coin/scene.gltf', //url to this model
+    scale: '0.5 0.5 0.5',
+    rotation: '0 225 0'
+  },
+];
+
+let modelIndex = 5; //set modelIndex to 0
+const setModel = (model, entity) => { //setModel acts like a function; takes the values of model and entity
+  if (model.position) {//if model has a position, then do the following:
+    entity.setAttribute('position', model.position); //set entity's position to that of model.position's, provided under const.model
+  }
+
+  entity.setAttribute('gltf-model', model.url); //set entity's model.url to the one provided under const.model
+};
+
+function render() { //render function
+  const scene = document.querySelector('a-scene'); //create a new instance of a scene which creates a new element under 'a-entity' in the index.html.  The contents of this a-entity are the following:
+
+  navigator.geolocation.getCurrentPosition(function (position) { //draws from the user's current position
+    const latitude = position.coords.latitude; //register the user's location as a variable latitutde
+    const longitude = position.coords.longitude; //register the user's location as a variable longitude
+
+    const model = document.createElement('a-entity'); //create an element within index.html under 'a-entity'
+    model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`); //set and write the longitude and latitude in 'a-entity'
+
+    setModel(models[modelIndex], model); //aset models based on the index number
+
+    model.setAttribute('animation-mixer', ''); //add 'animation-mixer' in the line
+
+    scene.appendChild(model); //adds a child of model to scene
+  });
+}
+
+//How the script uses objects in an objects oriented style//
+//Under the render function(), similar to the cookie-cutter method of object-oriented-probgramming, it constructs a 3D model object is being constructed; variables are passed through the equivalent of a constructor () : the latitude, longitude, and model URL.  This is then written into the index.html to be used.
+//Similar to OOP, I would use a loop and an array to load and create multiple 3D objects.  The code to handle them could be here and use the render function
+//I would add event listenrs in a seperate js file.  Buttons for interaction can be set in seperate html file with the event listeners wired to this html
---
+++ script.js
@@ -0,0 +1,38 @@
+window.onload = () => { //loads the following when the application is opened in a new window:
+    let places = staticLoadPlaces(); //declares variable 'places' as a new instance of the function staticLoadPlaces()
+    renderPlaces(places); //run the function in the window
+};
+
+function staticLoadPlaces() { //declaring the function staticloadPlaces()
+   return [ //this function will return the following:
+       {
+           name: 'MyModel', //a name property, which we set as Honour
+           location: { //the location:
+               lat: 43.641967, //lattitude coordinates
+               lng: -79.723553, //longitutde coordinates
+           }
+       },
+   ];
+}
+
+function renderPlaces(places) { //expand on what the function renderPlaces(places) from above will do:
+   let scene = document.querySelector('a-scene'); //declare the variable scene; its element uses document.querySelector, which returns the first Element(s) within the document that matches what we're looking for.  In this case, its 'a-scene'
+
+   places.forEach((place) => {//drawing from our index.html, for each value we receive from 'a-scene' using the document.querySelector, create a new instance of lattitude and longititude.  The below acts like a constructor:
+       let latitude = place.location.lat; //create a new instance of latitude which is stored using the structure of the previous function above
+       let longitude = place.location.lng; //create a new instance of longitude which is stored using the structure of the previous function above
+
+       let model = document.createElement('a-entity'); //create a new instance of a model which creates a new element under 'a-entity' in the index.html.  The contents of this a-entity are the following:
+       model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`); // set that model's gps-entity-place using the latittude and longitutde values declared and retrieved above
+       model.setAttribute('gltf-model', './assets/solgaleo_coin/scene.gltf'); //establish the 3D model to use.  Here, it will use a red sword .glb model.
+       model.setAttribute('rotation', '0 180 0'); //set up the rotation/position of the model in respects to xyz coordinates
+       model.setAttribute('animation-mixer', ''); //add animation-mixer
+       model.setAttribute('scale', '10 10 10'); //add the scale, here we set it to 5, 5, 5, by xyz dimensions.  So it will be smaller than the Honour card
+
+       model.addEventListener('loaded', () => { //addEventListener() takes an event t listen for, in this case 'loaded' and then fires another argument when 'loaded' gest fired
+           window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')) //within the window, a new event is then added to index.html; 'gps-entity-place-loaded'
+       });
+
+       scene.appendChild(model); //adds a child of model to scene
+   });
+}
